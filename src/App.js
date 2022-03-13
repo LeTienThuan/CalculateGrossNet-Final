220ce3e4d8dedd49d81ed43bb3edd75f20745589
unknown <you@example.com>
2022-03-14 00:47:16 +0700

    update modal overlay

--- src/App.js
+++ src/App.js
@@ -1,65 +1,71 @@
-import {useState} from "react";
+import { useState } from "react";
 import InputInfor from "./Components/InputInfor/InputInfor";
 import GroupButton from "./Components/Button/GroupButton";
 import DetailDescriptionTable from "./Components/ExplainDetail/DetailDescriptionTable";
 import "./App.css";
 import Result from "./Components/Result/Result";
-import {defaultInputValue} from "./DefaultValue/defaultInputValue";
-import {defaultResultDetailValue} from "./DefaultValue/defaultResultDetailValue";
-import {convertGrossToNet} from "./Logic/GrossToNet/Salary/convertGrossToNet";
-import {convertNetToGross} from "./Logic/NetToGross/Salary/convertNetToGross";
+import { defaultInputValue } from "./DefaultValue/defaultInputValue";
+import { defaultResultDetailValue } from "./DefaultValue/defaultResultDetailValue";
+import { convertGrossToNet } from "./Logic/GrossToNet/Salary/convertGrossToNet";
+import { convertNetToGross } from "./Logic/NetToGross/Salary/convertNetToGross";
 import classes from "./Components/CSS/General.module.css";
-import {getInsurancePercent} from "./Logic/GeneralFunction/getInsurance";
+import { getInsurancePercent } from "./Logic/GeneralFunction/getInsurance";
 
 function App() {
-    const [salaryValue, setSalaryValue] = useState(defaultInputValue);
-    const [resultDetail, setResultDetail] = useState(defaultResultDetailValue);
+  const [salaryValue, setSalaryValue] = useState(defaultInputValue);
+  const [resultDetail, setResultDetail] = useState(defaultResultDetailValue);
 
-    const {insurance} = salaryValue;
-    const {overallResult} = resultDetail;
-    const insurancePercent = getInsurancePercent(insurance);
+  const { insurance } = salaryValue;
+  const { overallResult } = resultDetail;
+  const insurancePercent = getInsurancePercent(insurance);
 
-    const updateIncome = (incomeValue) => {
-        setSalaryValue({...salaryValue, income: incomeValue})
-    }
-    const updateInsurance = (insuranceValue) => {
-        setSalaryValue({...salaryValue, insurance: insuranceValue})
-    }
-    const updateArea = (areaValue) => {
-        setSalaryValue({...salaryValue, area: areaValue})
-    }
-    const updateFamilyAllowances = (familyAllowances) => {
-        setSalaryValue({...salaryValue, familyAllowances: familyAllowances})
-    }
+  const updateIncome = (incomeValue) => {
+    setSalaryValue({ ...salaryValue, income: incomeValue });
+  };
+  const updateInsurance = (insuranceValue) => {
+    setSalaryValue({ ...salaryValue, insurance: insuranceValue });
+  };
+  const updateArea = (areaValue) => {
+    setSalaryValue({ ...salaryValue, area: areaValue });
+  };
+  const updateFamilyAllowances = (familyAllowances) => {
+    setSalaryValue({ ...salaryValue, familyAllowances: familyAllowances });
+  };
 
-    const calculateGrossToNet = () => {
-        const newResult = convertGrossToNet(salaryValue, resultDetail);
-        setResultDetail(newResult);
-    }
-    const calculateNetToGross = () => {
-        const newResult = convertNetToGross(salaryValue, resultDetail);
-        setResultDetail(newResult);
-    }
+  const calculateGrossToNet = () => {
+    const newResult = convertGrossToNet(salaryValue, resultDetail);
+    setResultDetail(newResult);
+  };
+  const calculateNetToGross = () => {
+    const newResult = convertNetToGross(salaryValue, resultDetail);
+    setResultDetail(newResult);
+  };
 
-    return (<>
-                <p className={classes.heading}>Tính lương Gross sang NET và ngược lại (cập nhật 1/1/2020)</p>
-                <div className="app">
-                    <InputInfor salaryValue={salaryValue}
-                                onUpdateIncome={updateIncome}
-                                onUpdateInsurance={updateInsurance}
-                                onUpdateArea={updateArea}
-                                onUpdateFamilyAllowances={updateFamilyAllowances}
-                    />
-                    <GroupButton onConvertGrossToNet={calculateGrossToNet}
-                                 onConvertNetToGross={calculateNetToGross}
-                    />
-                    <Result overallResult={overallResult}/>
-                    <DetailDescriptionTable result={resultDetail}
-                                            insurancePercent={insurancePercent}
-                    />
-                </div>
-            </>
-    );
+  return (
+    <>
+      <p className={classes.heading}>
+        Tính lương Gross sang NET và ngược lại (cập nhật 1/1/2020)
+      </p>
+      <div className="app">
+        <InputInfor
+          salaryValue={salaryValue}
+          onUpdateIncome={updateIncome}
+          onUpdateInsurance={updateInsurance}
+          onUpdateArea={updateArea}
+          onUpdateFamilyAllowances={updateFamilyAllowances}
+        />
+        <GroupButton
+          onConvertGrossToNet={calculateGrossToNet}
+          onConvertNetToGross={calculateNetToGross}
+        />
+        <Result overallResult={overallResult} />
+        <DetailDescriptionTable
+          result={resultDetail}
+          insurancePercent={insurancePercent}
+        />
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/Components/InputInfor/Area.js
+++ src/Components/InputInfor/Area.js
@@ -1,53 +1,72 @@
-import classes from '../CSS/General.module.css'
-import areaImg from '../Image/Area.png'
-import {useEffect, useState} from "react";
+import classes from "../CSS/General.module.css";
+import areaImg from "../Image/Area.png";
+import { useEffect, useState } from "react";
+import AreaModal from "./AreaModal";
 
 const Area = (props) => {
-    const [area, setArea] = useState(props.area);
+  const [area, setArea] = useState(props.area);
+  const [isShowModal, setIsShowModal] = useState(false);
 
-    useEffect(()=>{
-        props.onUpdateArea(area);
-    },[area])
+  useEffect(() => {
+    props.onUpdateArea(area);
+  }, [area]);
 
-    const handleArea = (event) => {
-        setArea(event.target.value)
-    }
+  const handleArea = (event) => {
+    setArea(event.target.value);
+  };
 
-    return (
-        <div className={`${classes['mt-10']} ${classes['fs-13']}`}>
-            <span>Vùng: &nbsp;
-                <img width='16px' src={areaImg} alt='small-icon'/> &nbsp;
-                <span>
-                    <input type='radio'
-                           value='1'
-                           checked={area === '1'}
-                           onChange={handleArea}
-                    /> I &nbsp;
-                </span>
-                <span>
-                    <input type='radio'
-                           value='2'
-                           checked={area === '2'}
-                           onChange={handleArea}
-                    /> II &nbsp;
-                </span>
-                <span>
-                    <input type='radio'
-                           value='3'
-                           checked={area === '3'}
-                           onChange={handleArea}
-                    /> III &nbsp;
-                </span>
-                <span>
-                    <input type='radio'
-                           value='4'
-                           checked={area === '4'}
-                           onChange={handleArea}
-                    /> IV &nbsp;
-                </span>
-            </span>
-        </div>
-    );
-}
+  return (
+    <div className={`${classes["mt-10"]} ${classes["fs-13"]}`}>
+      <span>
+        Vùng: &nbsp;
+        <img
+          className={classes["area-icon"]}
+          width="16px"
+          src={areaImg}
+          alt="small-icon"
+          //    onClick={}
+        />{" "}
+        &nbsp;
+        <AreaModal />
+        <span>
+          <input
+            type="radio"
+            value="1"
+            checked={area === "1"}
+            onChange={handleArea}
+          />{" "}
+          I &nbsp;
+        </span>
+        <span>
+          <input
+            type="radio"
+            value="2"
+            checked={area === "2"}
+            onChange={handleArea}
+          />{" "}
+          II &nbsp;
+        </span>
+        <span>
+          <input
+            type="radio"
+            value="3"
+            checked={area === "3"}
+            onChange={handleArea}
+          />{" "}
+          III &nbsp;
+        </span>
+        <span>
+          <input
+            type="radio"
+            value="4"
+            checked={area === "4"}
+            onChange={handleArea}
+          />{" "}
+          IV &nbsp;
+        </span>
+      </span>
+    </div>
+  );
+};
 
 export default Area;
